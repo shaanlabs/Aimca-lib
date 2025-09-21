c074baa62e6d69dccb8c49be4905bbf51dc73cde
shaanif ahmed <shaaniffakki@gmail.com>
2025-09-21 19:47:00 +0530

    Delete Aimca-lib-main directory

--- Aimca-lib-main/members/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.member_list, name='member_list'),
-    path('add/', views.add_member, name='add_member'),
-    path('<int:pk>/edit/', views.edit_member, name='edit_member'),
-    path('<int:pk>/delete/', views.delete_member, name='delete_member'),
-] 
